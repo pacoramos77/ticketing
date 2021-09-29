9cccb3df1b1e378deb1718fc4cd2c545daaf54ca
Francisco Ramos <franramos@outlook.com>
2021-09-29 13:06:09 +0200

    Auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/users/currentuser", (que, res) => {
-  res.send("Hi!!!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("Listing on port 3000");
