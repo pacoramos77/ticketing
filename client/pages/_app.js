87d9da3dd2e8d06e35bb3b69ef2854cc1b5e78ee
Francisco Ramos <franramos@outlook.com>
2021-10-02 15:13:05 +0200

    Client application

---
+++ client/pages/_app.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+const app = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default app;
---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function main(props) {
+  return (
+    <div>
+      <h1>main1</h1>
+    </div>
+  );
+}
+
+export default main;
