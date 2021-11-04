744cc1bbce8d380dc689d0ac1f1dad77582e25d4
Francisco Ramos <franramos@outlook.com>
2021-11-04 15:21:11 +0100

    changes

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -7,6 +7,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
+  console.log("Starting payments......");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -25,6 +25,7 @@ const start = async () => {
   }
 
   try {
+    console.log("Starting tickets......");
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
